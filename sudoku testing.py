dbc1dfdbd12bdef2e7c48fa5884f17b2da05299b
Alexis Gonzalez <alexis.gonzalez07@gmail.com>
2022-07-08 14:08:29 -0500

    Fixed variable naming to _ and method to CamelCase

--- sudoku testing.py
+++ sudoku testing.py
@@ -1,27 +1,27 @@
 import numpy as np
 class Board:
     def __init__(self, inp):
-        self.game_board = np.empty((9,9),dtype = object)
+        self.gameboard = np.empty((9,9),dtype = object)
         for x in range(9):
             for y in range(9):
-                self.game_board[x][y] = Slot(inp[9*x + y], x, y)
+                self.gameboard[x][y] = Slot(inp[9*x + y], x, y)
         self.row = [set(j for j in range(10)) for i in range(9)]
         self.grid = [set(j for j in range(10)) for i in range(9)]
         self.column = [set(j for j in range(10)) for i in range(9)]
                 
     def __str__(self):
-        boardString = ''
+        board_string = ''
         for i in range(9):
-            if i%3 == 0: boardString += '                     \n'; #to have a horizontal spacer between grids of nine
+            if i%3 == 0: board_string += '                     \n'; #to have a horizontal spacer between grids of nine
 
             for j in range(9):
-                if j%3 == 0: boardString += '   ';#to have vertical spacer between grids of nine
+                if j%3 == 0: board_string += '   ';#to have vertical spacer between grids of nine
 
-                boardString += f" {self.game_board[i][j]} "#fill in the box with the index of the grid at (row i,column j)
+                board_string += f" {self.gameboard[i][j]} "#fill in the box with the index of the grid at (row i,column j)
 
-            boardString += '\n'
+            board_string += '\n'
 
-        return boardString
+        return board_string
     
     
 #     def PossibleByLine(value,rowOrCol): #returns true when a value does not already exist in an inputted row or column numpy.array(slot)
@@ -29,25 +29,25 @@ class Board:
     
     def getRows(self):
         for x in range(9):
-            row_taken_values = {slot.value for slot in self.game_board[x]}
+            row_taken_values = {slot.value for slot in self.gameboard[x]}
             self.row[x] -= row_taken_values
             
         
     def getColumns(self):
         for x in range(9):
-            column_taken_values = {slot.value for slot in self.game_board[:,x]}
+            column_taken_values = {slot.value for slot in self.gameboard[:,x]}
             self.column[x] -= column_taken_values
         
     def getGrids(self):
         for x in range(3):
             for y in range(3):
-                flattened = self.game_board[3*x:3*x+3, 3*y:3*y+3].flatten()
+                flattened = self.gameboard[3*x:3*x+3, 3*y:3*y+3].flatten()
                 used_nums = {slot.value for slot in flattened}
                 self.grid[3*x+y] -= used_nums
 
     def updateBoard(self):
         for i in range(100):
-            for row in self.game_board:
+            for row in self.gameboard:
                 for slot in row:
                     if slot.value == 0:
                         slot.updatePossibleValues(self.row[slot.row], self.column[slot.column], self.grid[slot.grid])
@@ -62,7 +62,7 @@ class Board:
             
             
     # def nextMove(self):
-    #     for slot in self.game_board:
+    #     for slot in self.gameboard:
     #         if len(slot.possible_values) == 1:
     #             self.updaterowsandgridandcolumn(slot)
     #         elif len(slot.possibleValues) > 1:
