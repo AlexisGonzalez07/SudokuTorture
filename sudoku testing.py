c4e92aa0ab6de864c0bafd76a1628ba11070a425
James D'Imperio <jamesdimperio@hotmail.com>
2022-07-11 12:28:58 -0400

    Moved around and added some functions to imporve readability, implemented recursion into the steps the algorithm takes, caused it to stop when solved or no more possible moves can be made, and cleaned up commented/unused code

--- sudoku testing.py
+++ sudoku testing.py
@@ -1,7 +1,7 @@
 import numpy as np
 class Board:
     def __init__(self, inp):
-        self.gameboard = np.empty((9,9),dtype = object)
+        self.game_board = np.empty((9,9),dtype = object)
         for x in range(9):
             for y in range(9):
 
@@ -22,15 +22,15 @@ class Board:
             for j in range(9):
                 if j%3 == 0: board_string += '   ';#to have vertical spacer between grids of nine
 
-                board_string += f" {self.gameboard[i][j]} "#fill in the box with the index of the grid at (row i,column j)
+                board_string += f" {self.game_board[i][j]} "#fill in the box with the index of the grid at (row i,column j)
 
             board_string += '\n'
 
         return board_string
 
 
-#     def PossibleByLine(value,rowOrCol): #returns true when a value does not already exist in an inputted row or column numpy.array(slot)
-#         return filter(lambda rowOrColVal: rowOrColVal != value,rowOrCol)
+    def PossibleByLine(slot,rowOrCol): #returns true when a value does not already exist in an inputted row or column numpy.array(slot)
+        return filter(lambda rowOrColVal: rowOrColVal != slot.value,rowOrCol)
 
     def repGrids(self):
         ret_grids = np.empty((9,9),dtype = object)
@@ -58,22 +58,27 @@ class Board:
                 self.poss_in_grids[3*x+y] -= used_nums
 
     def updateBoard(self):
-        for i in range(100):
-            for row in self.game_board:
-                for slot in row:
-                    if slot.value == 0:
-                        slot.updatePossibleValues(self.poss_in_rows[slot.row], self.poss_in_columns[slot.column], self.poss_in_grids[slot.grid])
-                        if slot.value != 0:
-                            self.poss_in_rows[slot.row] -= {slot.value}
-                            self.poss_in_columns[slot.column] -= {slot.value}
-                            self.poss_in_grids[slot.grid] -= {slot.value}
-            self.checkSectionUnique(self.game_board)
-            self.checkSectionUnique(np.transpose(self.game_board))
-            self.checkSectionUnique(self.grid_rep)
+        did_update = False
+        for row in self.game_board:
+            for slot in row:
+                if slot.value == 0:
+                    slot.updatePossibleValues(self.poss_in_rows[slot.row], self.poss_in_columns[slot.column], self.poss_in_grids[slot.grid])
+                    if slot.value != 0:
+                        self.poss_in_rows[slot.row] -= {slot.value}
+                        self.poss_in_columns[slot.column] -= {slot.value}
+                        self.poss_in_grids[slot.grid] -= {slot.value}
+                        did_update = True
+        if did_update :
+            self.updateBoard()
+        elif self.isSolved():
+            print(self)    
+        else:
+            self.secondAlgo()
 
-    def checkSectionUnique(self, section):
+    def checkSectionUnique(self, section, poss_in_section):
+        did_update = False
         for i in range(9):
-            for num in self.poss_in_rows[i]:
+            for num in poss_in_section[i].copy():
                 count = 0
                 slot_for_num = None
                 for slot in section[i]:
@@ -81,52 +86,33 @@ class Board:
                         count += 1
                         slot_for_num = slot
                 if count == 1:
-                    slot_for_num.value = num
-                    slot_for_num.possible_values = {0}                       
+                    slot_for_num.updateSlot(num)
+                    self.poss_in_rows[slot_for_num.row] -= {num}
+                    self.poss_in_columns[slot_for_num.column] -= {num}
+                    self.poss_in_grids[slot_for_num.grid] -= {num}
+                    did_update = True               
+        if did_update:
+            self.updateBoard()
 
-            
-    # def nextMove(self):
-    #     for slot in self.game_board:
-    #         if len(slot.possible_values) == 1:
-    #             self.updaterowsandgridandcolumn(slot)
-    #         elif len(slot.possibleValues) > 1:
-    #             slot.updatePossibleValues(row[self.row], column[self.column], grid[self.grid])
-            
-    #     self.nextMove()
-       #    def updateBoard(self):
-#        didUpdate = False
- #       for row in self.gameboard:
- #           for slot in row:
- #               if slot.value == 0:
- #                  slot.updatePossibleValues(self.row[slot.row], self.column[slot.column], self.grid[slot.grid])
- #                   if slot.value != 0:
- #                       didUpdate = True
- #                       self.row[slot.row] -= {slot.value}
- #                       self.column[slot.column] -= {slot.value}
-#                        self.grid[slot.grid] -= {slot.value}
-#        if didUpdate:
- #           self.updateBoard()
- #       elif self.isSolved():
- #           print(self)
- #       else:
- #           self.secondAlgo()
 
     def secondAlgo(self):
-        pass
-        #check for zeros in the matrix
-        #if there are some zeros we can try part two of our algorithm
+        self.checkSectionUnique(self.game_board, self.poss_in_rows)
+        self.checkSectionUnique(np.transpose(self.game_board), self.poss_in_columns)
+        self.checkSectionUnique(self.grid_rep, self.poss_in_grids)
 
-        #     if didUpdate == false:
-        #     r#un our second algorithim
-        # else:
-        #     updateBoard()
+    # def thirdAlgo:
+    #     short list length possible = 2
+    #     shortlist[0]: {1, 5}
+    #     updateBoard();
 
-        #didUpdate = false 2*, we stop our recursion
+    def isSolvable(self):
+
+        return True
 
     def isSolved(self):
         for i in range(9):
             for j in range(9):
-                if self.gameboard[i][j] == 0:
+                if self.game_board[i][j].value == 0:
                     return False
         return True
                 
@@ -140,15 +126,9 @@ class Slot:
         self.grid = 3 * (x//3) + (y//3)
         self.value = int(value)
         self.possible_values = {1,2,3,4,5,6,7,8,9}
+
     def __str__(self):
         return str(self.value)
-
-    def getValue(self):
-        return self.value
-
-    def getPossibleValues(self):
-        return self.possible_values
-
     
     def updateSlot(self,value):
         self.value = value
@@ -161,15 +141,19 @@ class Slot:
             self.updateSlot(next(iter(self.possible_values)))
 
 
-
+import time
 #inp_game = "800504013100000600002010570407000905500420000000059460081002000000975182000001000"
+inp_game = "100097000000030008609500003000000640001000000048079005930010006000000000004700530"
+#inp_game = "000010000035000040008094036600070000000309450000008000009000700000700200403002000"
+start = time.time()
+game_board = Board(inp_game)
+print(game_board)
+game_board.getRows()
+game_board.getColumns()
+game_board.getGrids()
+game_board.updateBoard()
 
-inp_game = "000010000035000040008094036600070000000309450000008000009000700000700200403002000"
-gameboard = Board(inp_game)
-
-gameboard.getRows()
-gameboard.getColumns()
-gameboard.getGrids()
-gameboard.updateBoard()
 print("==========================================")
-print(gameboard)
+print(game_board)
+end = time.time()
+print(end-start)
