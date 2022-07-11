e8d5e8f9f29171a1a60cacdf3834aaeff6d3c201
James DImperio <jamesdimperio@hotmail.com>
2022-07-11 10:36:24 -0400

    James branch (#6)
    * Wrote getGrids, tested on easy board and it worked, temporarily have the board do 100 moves and finish whether it is done or not
    
    * Wrote function for second part of algorithm called 'CheckSectionUnique' that checks each slot agains each row column and grid to see if there is only one slot that an element can go in
    
    Co-authored-by: VICTOR LUPASCU <victor.lupascu.1985@gmail.com>

--- sudoku personal testing.py
+++ sudoku personal testing.py
@@ -1,10 +1,9 @@
 import numpy as np
 class Board:
     def __init__(self, inp):
-        self.gameboard = np.empty((9,9),dtype = object)
+        self.game_board = np.empty((9,9),dtype = object)
         for x in range(9):
             for y in range(9):
-
                 self.game_board[x][y] = Slot(inp[9*x + y], x, y)
                 if inp[9*x+y] != 0:
                     self.game_board[x][y].possible_values = {0}
@@ -13,25 +12,23 @@ class Board:
         self.poss_in_columns = [set(j for j in range(10)) for i in range(9)]
         self.grid_rep = self.repGrids()
 
-
     def __str__(self):
-        board_string = ''
+        boardString = ''
         for i in range(9):
-            if i%3 == 0: board_string += '                     \n'; #to have a horizontal spacer between grids of nine
+            if i%3 == 0: boardString += '                     \n'; #to have a horizontal spacer between grids of nine
 
             for j in range(9):
-                if j%3 == 0: board_string += '   ';#to have vertical spacer between grids of nine
+                if j%3 == 0: boardString += '   ';#to have vertical spacer between grids of nine
 
-                board_string += f" {self.gameboard[i][j]} "#fill in the box with the index of the grid at (row i,column j)
+                boardString += f" {self.game_board[i][j]} "#fill in the box with the index of the grid at (row i,column j)
 
-            board_string += '\n'
+            boardString += '\n'
 
-        return board_string
-
-
+        return boardString
+    
+    
 #     def PossibleByLine(value,rowOrCol): #returns true when a value does not already exist in an inputted row or column numpy.array(slot)
 #         return filter(lambda rowOrColVal: rowOrColVal != value,rowOrCol)
-
     def repGrids(self):
         ret_grids = np.empty((9,9),dtype = object)
         for i in range(3):
@@ -82,8 +79,39 @@ class Board:
                         slot_for_num = slot
                 if count == 1:
                     slot_for_num.value = num
-                    slot_for_num.possible_values = {0}                       
+                    slot_for_num.possible_values = {}
 
+                        
+    def checkColsUnique(self):
+        for i in range(9):
+            for num in self.poss_in_columns[i]:
+                count = 0
+                slot_for_num = None
+                for slot in self.game_board[:,i]:
+                    if slot.value == 0 and num in slot.possible_values:
+                        count += 1
+                        slot_for_num = slot
+                if count == 1:
+                    slot_for_num.value = num
+                    slot_for_num.possible_values = {}
+
+    def checkGridsUnique(self):
+        for i in range(9):
+            if i == 1:
+                print(self.poss_in_grids[i])
+            for num in self.poss_in_grids[i]:
+                count = 0
+                slot_for_num = None
+                for slot in self.grid_rep[i]:
+                    if slot.value == 0 and num in slot.possible_values:
+                        count += 1
+                        slot_for_num = slot
+                if count == 1:
+                    slot_for_num.value = num
+                    slot_for_num.possible_values = {}
+
+                
+            
             
     # def nextMove(self):
     #     for slot in self.game_board:
@@ -93,83 +121,42 @@ class Board:
     #             slot.updatePossibleValues(row[self.row], column[self.column], grid[self.grid])
             
     #     self.nextMove()
-       #    def updateBoard(self):
-#        didUpdate = False
- #       for row in self.gameboard:
- #           for slot in row:
- #               if slot.value == 0:
- #                  slot.updatePossibleValues(self.row[slot.row], self.column[slot.column], self.grid[slot.grid])
- #                   if slot.value != 0:
- #                       didUpdate = True
- #                       self.row[slot.row] -= {slot.value}
- #                       self.column[slot.column] -= {slot.value}
-#                        self.grid[slot.grid] -= {slot.value}
-#        if didUpdate:
- #           self.updateBoard()
- #       elif self.isSolved():
- #           print(self)
- #       else:
- #           self.secondAlgo()
-
-    def secondAlgo(self):
-        pass
-        #check for zeros in the matrix
-        #if there are some zeros we can try part two of our algorithm
-
-        #     if didUpdate == false:
-        #     r#un our second algorithim
-        # else:
-        #     updateBoard()
-
-        #didUpdate = false 2*, we stop our recursion
-
-    def isSolved(self):
-        for i in range(9):
-            for j in range(9):
-                if self.gameboard[i][j] == 0:
-                    return False
-        return True
+       
                 
 class Slot:
-
-
-
     def __init__(self, value, x,y):
         self.row = x
         self.column = y
         self.grid = 3 * (x//3) + (y//3)
         self.value = int(value)
         self.possible_values = {1,2,3,4,5,6,7,8,9}
+    
     def __str__(self):
         return str(self.value)
-
+    
     def getValue(self):
         return self.value
-
+    
     def getPossibleValues(self):
         return self.possible_values
-
     
     def updateSlot(self,value):
         self.value = value
         self.possible_values = {0}
     
-
     def updatePossibleValues(self, row_values, column_values, grid_values):
         self.possible_values = set.intersection(row_values, column_values, grid_values)
         if len(self.possible_values) == 1:
             self.updateSlot(next(iter(self.possible_values)))
 
 
-
-#inp_game = "800504013100000600002010570407000905500420000000059460081002000000975182000001000"
-
-inp_game = "000010000035000040008094036600070000000309450000008000009000700000700200403002000"
+inp_game = "800504013100000600002010570407000905500420000000059460081002000000975182000001000"
 gameboard = Board(inp_game)
-
+print(gameboard)
 gameboard.getRows()
 gameboard.getColumns()
 gameboard.getGrids()
 gameboard.updateBoard()
 print("==========================================")
 print(gameboard)
+
